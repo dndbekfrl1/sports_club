2b532386767492640d5acd9990e2c6459e706491
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-08-11 01:53:22 +0900

    Update mypage.js

--- js/mypage.js
+++ js/mypage.js
@@ -1,7 +1,16 @@
 $(function(){
-    $(".tab ul li").click(function(){
-        $(".tab ul li").removeClass('on');
-        $(".tab .conBox").removeClass('on');
+    $(".mytab ul li").click(function(){
+        $(".mytab ul li").removeClass('on');
+        $(".mytab .conBox").removeClass('on');
+        $(this).addClass('on');
+        $("#"+$(this).data('id')).addClass('on');
+    });
+});
+
+$(function(){
+    $(".clubtab ul li").click(function(){
+        $(".clubtab ul li").removeClass('on');
+        $(".clubtab .clubconBox").removeClass('on');
         $(this).addClass('on');
         $("#"+$(this).data('id')).addClass('on');
     });
