2a2ef13f3dab2908212ab6f4a2049a8a10cde544
Moon <swb3085@naver.com>
2020-08-26 14:58:23 +0900

    작성자 : 문영환 header 메뉴창과 자바스크립트 동작 추가 toggle기능 등 자바스크립트 추가

---
+++ sportsClub/js/test.js
@@ -0,0 +1,10 @@
+
+
+window.onload = function (){
+    function MyTest(props){
+        return <h1>SUCCESS</h1>
+    }
+
+    ReactDOM.render(<MyTest></MyTest>, document.getElementById('test1'));
+}
+
